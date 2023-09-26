348bdc6a9fe616a94d8f140003d73e206a9815af
Henry <henrybbaker20@gmail.com>
2023-09-25 23:11:26 -0700

    Fixed a bug with the sink() func to correct swap conditions, as well as added comments and cleaned style for improved readability.

--- src/PriorityQueue.java
+++ src/PriorityQueue.java
@@ -1,24 +1,50 @@
 /**
+ * Implementation of a PriorityQueue class via an implicit heap structure.
+ * The structure uses an ArrayList to dynamically resize itself and is generally
+ * more flexible than a standard array, which is useful for this academic project.
  *
+ * @author Henry Baker
+ * @version 2023.09.25
  */
 
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class PriorityQueue<E> {
+
+    /**
+     * Helper class object that holds the data to be
+     * stored and the priority of that data in the
+     * PriorityQueue.
+     */
     private class Node {
         private final E data;
         private int priority;
 
+        /**
+         * Constructor for Node
+         *
+         * @param inputObj Element to be stored.
+         * @param score int priority of that element.
+         */
         public Node(E inputObj, int score) {
             data = inputObj;
             priority = score;
         }
 
+        /**
+         * Accessor method for data.
+         * @return Data stored in Node.
+         */
         public E getData() {
             return data;
         }
 
+        /**
+         * Accessor for Node priority.
+         *
+         * @return int priority of Node
+         */
         public int getPriority() {
             return priority;
         }
@@ -31,16 +57,34 @@ public class PriorityQueue<E> {
 
 
     // Constructors
+
+    /**
+     * Default constructor creates a PriorityQueue that prioritizes 0-base
+     * as the maximum priority value.
+     */
     public PriorityQueue() {
         queueHeap = new ArrayList<Node>();
     }
 
+    /**
+     * Constructor that creates a PriorityQueue that prioritizes higher
+     * priority values higher.
+     *
+     * @param highFirst
+     */
     public PriorityQueue(boolean highFirst) {
         queueHeap = new ArrayList<Node>();
         hiFirst = highFirst;
     }
 
     //Public PriorityQueue methods
+
+    /**
+     * Adds an element to the priority queue at the specified priority value.
+     *
+     * @param object Element to be added to the queue.
+     * @param score int priority of the element to be added.
+     */
     public void add(E object, int score) {
         // Create new node with object and score
         Node newNode = new Node(object, score);
@@ -50,15 +94,29 @@ public class PriorityQueue<E> {
         swim();
     }
 
+    /**
+     * Clears the priority queue.
+     */
     public void clear() {
         queueHeap = null;
         queueHeap = new ArrayList<Node>();
     }
 
+    /**
+     * Returns the size of the priority queue.
+     *
+     * @return int size of the priority queue.
+     */
     public int size() {
         return queueHeap.size();
     }
 
+    /**
+     * Returns whether the queue contains the specified element.
+     *
+     * @param object Object to check if contains.
+     * @return Boolean if queue contains element.
+     */
     public boolean contains(E object) {
         for(Node n : queueHeap) {
             if(n.getData() == object) { // Make sure that you can use == method for comparison
@@ -68,6 +126,11 @@ public class PriorityQueue<E> {
         return false;
     }
 
+    /**
+     * Removes and returns the element of highest priority in the queue.
+     *
+     * @return Element of highest priority in the queue.
+     */
     public E getNext() {
         // swap head and end, sink head, end in temp var, remove end, return temp
         Node temp = queueHeap.get(0);
@@ -78,18 +141,47 @@ public class PriorityQueue<E> {
         return temp.getData();
     }
 
+    /**
+     * Returns if priority queue is empty.
+     *
+     * @return Boolean if queue is empty.
+     */
     public boolean isEmpty() {
         return queueHeap.isEmpty();
     }
 
+    /**
+     * Returns the element in the queue with the highest priority
+     * without removing it from the list.
+     *
+     * @return Element of highest priority.
+     */
     public E peek() {
-        return null;
+        return queueHeap.get(0).getData();
     }
 
+    /**
+     * Removes a specified object from the queue
+     * Only removes the first occurrence in the list.
+     *
+     * @param object Object to be removed from the list.
+     * @return Boolean if removal is successful.
+     */
     public boolean remove(E object) {
+        for(Node n: queueHeap) {
+            if (n.getData() == object) {
+                queueHeap.remove(n);
+                return true;
+            }
+        }
         return false;
     }
 
+    /**
+     * The toString method creates a printable list of all elements in the queue.
+     *
+     * @return String list of all elements in the priority queue.
+     */
     public String toString() {
         StringBuilder queueString = new StringBuilder("[");
         int queueSize = queueHeap.size();
@@ -106,51 +198,88 @@ public class PriorityQueue<E> {
         return queueString.toString();
     }
 
-    //Private Heap-related methods
+    // Private Heap-related methods
     // Sinking and Swimming insights found at: https://mathcenter.oxford.emory.edu/site/cs171/sinkAndSwim/
+
+    // *************************
+    //
+    //  Sink() method moves top index down through the heap data structure in order to maintain
+    //  heap structure property on a getNext() call.
+    //  Additional info on how swim() works is provided in the code.
+    //
+    // *************************
     private void sink() {
         // heap sink() function, called when removing from the PQ
+        // Start at index 0
         int i = 0;
 
+        // While the left child index is leq to the last index of the arrayList
         while(((2*i)+1) <= queueHeap.size()-1){
 
+            // Start with left child
             int j = (2*i)+1;
-            if(j < queueHeap.size()-1 && compare(queueHeap.get(j), queueHeap.get(j+1))) {
+
+            // If the left child is less than the last index (then a right child must exist)
+            // && the left child is NOT higher priority, then set j to the index of right child
+            if(j < queueHeap.size()-1 && !compare(queueHeap.get(j), queueHeap.get(j+1))) {
                 j++;
             }
-            if(compare(queueHeap.get(j), queueHeap.get(i))) {
+            // if winning child is lower priority than current parent, break and don't swap
+            if(compare(queueHeap.get(i), queueHeap.get(j))) {
                 break;
             }
+            // Otherwise, swap the elements and set the new parent index to the winning child containing sinking node
             swap(j, i);
             i = j;
 
         }
     }
 
+    // *************************
+    //
+    //  Swim() func takes a Node added to last element of queueHeap
+    //  and moves it up through the tree structure to maintain heap structure
+    //  property on an add() call.
+    //
+    // *************************
     private void swim() {
         // heap swim function, called when inserting to the PQ
 
         int i = queueHeap.size()-1;
 
+        // While node is not the root
         while (i > 0) {
+            // Get parent index
             int j = (i-1)/2;
+            // if the current index of swimming item is higher priority,
             if(compare(queueHeap.get(i),queueHeap.get(j))) {
+                // then swap it with its parent
                 swap(i, j);
+                // Set the swimming index counter to that of its parent
                 i = j;
-                //System.out.println(i);
             } else {
-                //System.out.println("No swap, i= "+i);
                 break;
             }
         }
     }
 
+    // *************************
+    //
+    //  Swaps nodes at index i and j of the queueHeap.
+    //
+    // *************************
     private void swap(int i, int j) {
         Collections.swap(queueHeap, i, j);
     }
 
+    // *************************
+    //
+    //  Returns boolean Node a greater priority than Node b
+    //  By default, compare() returns true for 0 being max priority.
+    //  If hiFirst is true, then higher value priority is true.
+    //
+    // *************************
     private boolean compare(Node a, Node b) {
-        // Check this logic once sink and swim are implemented, this could be wrong
         if(hiFirst) {
             return (a.getPriority() > b.getPriority());
         }
--- src/PriorityQueueTest.java
+++ src/PriorityQueueTest.java
@@ -4,8 +4,10 @@ public class PriorityQueueTest {
     public static void main(String[] args) {
         PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
 
+        // Test print on empty List
         System.out.println("Empty pQueue: " + queue.toString());
 
+        // Test print on 4 add()'s
         queue.add(1, 6);
 
         System.out.println("Size 1 pQueue: " + queue.toString());
@@ -22,20 +24,21 @@ public class PriorityQueueTest {
 
         System.out.println("Size 4 pQueue: " + queue.toString());
 
+        // Test clear() queue
         queue.clear();
 
         System.out.println("Cleared pQueue: " + queue.toString());
 
+        // Test contains()
         int i = 15;
 
         queue.add(i, 7);
-
-        System.out.println("Size 1 pQueue: " + queue.toString());
 
         boolean containsI = queue.contains(i);
 
         assert containsI;
 
+        // Test size()
         queue.add(1, 5);
 
         queue.add(5, 3);
@@ -44,11 +47,63 @@ public class PriorityQueueTest {
 
         System.out.println("pQueue: " + queue.toString());
 
+        // Test getNext();
         int j = queue.getNext();
 
-        System.out.println("Highest Priority item should be 5: " + j);
+        assert j == 5;
 
         System.out.println("pQueue: " + queue.toString());
+
+        queue.add(1, 6);
+        queue.add(2, 4);
+        queue.add(3, 7);
+        queue.add(4, 2);
+
+        // Test peek()
+        int peekInt = queue.peek();
+
+        System.out.println("pQueue: " + queue.toString());
+
+        assert peekInt == 4;
+
+        // Test remove
+        queue.add(4, 8);
+
+        assert queue.remove(4);
+
+        assert queue.remove(4);
+
+        assert !queue.remove(4);
+
+        assert queue.remove(3);
+
+        assert !queue.remove(8);
+
+        System.out.println("pQueue: " + queue.toString());
+
+        PriorityQueue<String> stringPQ = new PriorityQueue<String>();
+
+        stringPQ.add("World", 1);
+        stringPQ.add("Hello", 0);
+
+        System.out.println(stringPQ.toString());
+
+        stringPQ.clear();
+
+        stringPQ.add("C", 6);
+        stringPQ.add("D", 2);
+        stringPQ.add("C", 6);
+        stringPQ.add("Z", 0);
+
+        System.out.println(stringPQ.toString());
+
+        stringPQ.getNext();
+
+        System.out.println(stringPQ.toString());
+
+
+
+
 
     }
 }
